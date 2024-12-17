16e15ca4db4d2b28d6cf6f064e7762956c76cdc5
Sagarjainjain <sagarjain.jain10@gmail.com>
2024-12-17 11:24:32 +0530

    error fixed

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -1,17 +1,15 @@
-
 import nodemailer from "nodemailer";
-import dotenv from "dotenv"
-
+import dotenv from "dotenv";
+dotenv.config();
 
 export const POST = async (req, res) => {
   const { emailfrom, name, message } = await req.json();
-  dotenv.config();
   const emailto = "teakaybee97@gmail.com";
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: process.env.EMAIL,
-      pass: process.env.EMAIL_PASSWORD, // Change "password" to "pass" (correct property)
+      user: emailto,
+      pass: "fmwmarpjoezfjvdx", 
     },
     tls: {
       rejectUnauthorized: false, // This allows using self-signed certificates
@@ -20,12 +18,12 @@ export const POST = async (req, res) => {
 
   try {
     await transporter.sendMail({
-      
       from: emailfrom,
       to: emailto,
-      subject: `New Message from ${name}`,
-      text: `${name} Email: ${emailfrom} & Message: ${message}`,
+      subject: `New Message from ${name}`, // Use backticks
+      text: `${name} Email: ${emailfrom} & Message: ${message}`, // Use backticks
     });
+
     return new Response(
       JSON.stringify({ message: "Email sent Successfully" }),
       { status: 200 }
